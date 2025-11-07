4dd45d437324be74234b51685cd6671af4d4d133
Dumitru-Bobeico <dima.bobeico.2003@gmail.com>
2025-11-07 14:12:54 +0200

    fix

--- cooking_app/lib/controllers/recipe_controller.dart
+++ cooking_app/lib/controllers/recipe_controller.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:get/get.dart';
+import '../v2.dart';
 import '../models/recipe.dart';
 
 class RecipeController extends GetxController {
@@ -15,7 +16,11 @@ class RecipeController extends GetxController {
 
   Future<void> loadRecipeData() async {
     try {
-      final String response = await rootBundle.loadString('assets/data.json');
+      final data = {
+        "list": list,
+        "details": details,
+      };
+      final String response = jsonEncode(data);
       recipeData.value = RecipeData.fromRawJson(response);
     } catch (e) {
       print("Error loading recipe data: $e");
